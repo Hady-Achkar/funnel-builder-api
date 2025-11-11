29fcc3c614864a95ad7720ac378fb5e068102517
ahmadalmuhtar2 <ahmadalmuhtarsyria@gmail.com>
2025-11-11 11:25:38 +0100

    feat: remove 'maximumWorkspaces' column from users table and drop default for itemType in subscriptions

--- prisma/migrations/20251110085338_submissions/migration.sql
+++ prisma/migrations/20251110085338_submissions/migration.sql
@@ -1,11 +1,2 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `maximumWorkspaces` on the `users` table. All the data in the column will be lost.
-
-*/
 -- AlterTable
 ALTER TABLE "public"."subscriptions" ALTER COLUMN "itemType" DROP DEFAULT;
-
--- AlterTable
-ALTER TABLE "public"."users" DROP COLUMN "maximumWorkspaces";
